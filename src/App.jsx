e2b20c31421a78289aeca4fc13fb5e6fea1eea8f
Thanh Tran <93765763+tranthanhvi@users.noreply.github.com>
2024-01-18 18:56:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Home from './component/Home';
-import Todolist from './components/Todolist';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Home from './Home';
+import Todolist from './Todolist';
 import './App.css'
 
 function App() {
 
-
   return (
-<>
+    <>
+<BrowserRouter>
+
+    <nav>
+      <Link to="/">Home</Link>{' '}
+      <Link to="/todolist">Todolist</Link>{' '}
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/todolist" element={<Todolist />} />
+    </Routes>
+</BrowserRouter>
+
     </>
   );
   
