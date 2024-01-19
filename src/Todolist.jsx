031b4b611b9159bac76c88134ba2bb5966685d7a
Thanh Tran <93765763+tranthanhvi@users.noreply.github.com>
2024-01-19 15:04:31 +0200

    Update Todolist.jsx with MUI components

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,46 +1,66 @@
-import { useState } from "react";
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import './App.css';
 
-function Todolist() {
+function App() {
+  const [todo, setTodo] = useState({description: '', date: ''});
+  const [todos, setTodos] = useState([]);
 
-    const [todo, setTodo] = useState({description: '', date: ''});
-    const [todos, setTodos] = useState([]);
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
 
-    const inputChanged = (event) => {
-        setTodo({...todo, [event.target.name]: event.target.value})
-    } 
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+  }
 
-    const addTodo = () => {
-        setTodos([...todos, todo]);
-        setTodo({description: '', date: ''});
-    
-    }
+  const deleteTodo = (row) => {
+    setTodos(todos.filter((todo, index) => index !== row));
+  }
 
-    const deleteTodo = (row) => {
-        setTodos(todos.filter((todo, index) => row != index))
-    }
-
-
-    return(
+  return (
     <>
-<input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
-<input placeholder="Date" name="date" value={todo.date} onChange={inputChanged} />
-<input type="button" value="Add" onClick={addTodo} />
-<table>
-    <tbody>
-{todos.map((todo, index) => 
 
-<tr key={index}>
-    <td>{todo.description}</td>
-    <td>{todo.date}</td>
-    <input type="button" value="Delete" onClick= {() => deleteTodo(index)} />
-</tr>)
-}
-</tbody>
-</table>
+  <AppBar position='static'>
+    <Toolbar>
+      <Typography variant='h6'>
+        Todolist
+      </Typography>
+    </Toolbar>
+  </AppBar>
+      <Stack 
+  direction="row" 
+  spacing={2} 
+  mt={2} 
+  justifyContent="center"
+  alignItems="center">
 
+      <TextField variant="standard" label="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <TextField variant="standard" label="Date" name="date" value={todo.date} onChange={inputChanged}/>
+      <Button variant='outlined' onClick={addTodo}>Add</Button> 
+
+      </Stack>
+      <table>
+        <tbody>
+       {
+          todos.map((todo, index) => 
+            <tr key={index}>
+              <td>{todo.description}</td>
+             <td>{todo.date}</td>
+             <td><IconButton size="small" color="error" onClick={() => deleteTodo(index)}><DeleteIcon /></IconButton></td>
+            </tr>)
+        }
+        </tbody>
+      </table>
     </>
-    );
+  );
 }
 
-export default Todolist;
+export default App;
